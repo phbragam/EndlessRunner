3b4048e886bf7acbd9fb87520bde10cc506b240a
Pedro Braga <phbm.braga@gmail.com>
2021-10-31 23:25:33 -0300

    touch input handler adicionado

---
+++ Assets/_Project/Scripts/Input/InputManager.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[DefaultExecutionOrder(-1)]
+public sealed class InputManager : MonoBehaviour
+{
+    public event Action<Vector2, float> OnStartTouch;
+    public event Action<Vector2, float> OnEndTouch;
+    //public delegate void StartTouch(Vector2 position, float time);
+    // precisa do event?
+    //public event StartTouch OnStartTouch;
+    //public delegate void EndTouch(Vector2 position, float time);
+    //public event EndTouch OnEndTouch;
+
+    public static InputManager Instance;
+    private TouchControls _touchControls;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        _touchControls = new TouchControls();
+    }
+
+    private void OnEnable()
+    {
+        _touchControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _touchControls.Disable();
+    }
+
+    void Start()
+    {
+        _touchControls.Touch.PrimaryContact.started += ctx => StartTouchPrimary(ctx);
+        _touchControls.Touch.PrimaryContact.canceled += ctx => EndTouchPrimary(ctx);
+    }
+
+    private void StartTouchPrimary(InputAction.CallbackContext context)
+    {
+        OnStartTouch?.Invoke(_touchControls.Touch.PrimaryPosition.ReadValue<Vector2>(), (float)context.startTime);
+    }
+
+    private void EndTouchPrimary(InputAction.CallbackContext context)
+    {
+        OnEndTouch?.Invoke(_touchControls.Touch.PrimaryPosition.ReadValue<Vector2>(), (float)context.time);
+    }
+
+
+
+
+}
---
+++ Assets/_Project/Scripts/Input/SwipeDetection.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour
+{
+    [SerializeField] private float minimumDistance = .2f;
+    [SerializeField] private float maximumTime = 1f;
+    [SerializeField, Range(0f, 1f)] private float directionThreshold = .9f;
+
+    private InputManager _inputManager;
+
+    private Vector2 _startPosition;
+    private float _startTime;
+    private Vector2 _endPosition;
+    private float _endTime;
+
+    private void Awake()
+    {
+        _inputManager = InputManager.Instance;
+    }
+
+    private void OnEnable()
+    {
+        _inputManager.OnStartTouch += SwipeStart;
+        _inputManager.OnEndTouch += SwipeEnd;
+    }
+
+    private void OnDisable()
+    {
+        _inputManager.OnStartTouch -= SwipeStart;
+        _inputManager.OnEndTouch -= SwipeEnd;
+    }
+
+    private void SwipeStart(Vector2 position, float time)
+    {
+        _startPosition = position;
+        _startTime = time;
+    }
+
+    private void SwipeEnd(Vector2 position, float time)
+    {
+        _endPosition = position;
+        _endTime = time;
+        DetectSwipe();
+    }
+
+    private void DetectSwipe()
+    {
+        if (Vector2.Distance(_startPosition, _endPosition) >= minimumDistance
+            && (_endTime - _startTime) <= maximumTime)
+        {
+            //Debug.Log("Swipe detected");
+            Debug.DrawLine(_startPosition, _endPosition, Color.red, 5f);
+            Vector2 direction = (_endPosition - _startPosition).normalized;
+            SwipeDirection(direction);
+        }
+    }
+
+    private void SwipeDirection(Vector2 direction)
+    {
+        if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
+        {
+            Debug.Log("Swipe Up");
+        }
+        else if (Vector2.Dot(Vector2.left, direction) > directionThreshold)
+        {
+            Debug.Log("Swipe Left");
+        }
+        else if (Vector2.Dot(Vector2.right, direction) > directionThreshold)
+        {
+            Debug.Log("Swipe Right");
+        }
+    }
+
+
+}
