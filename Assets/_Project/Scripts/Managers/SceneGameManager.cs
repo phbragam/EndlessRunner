0288b86b03671040c9c8d28a62f88c9df29ab040
Pedro Braga <phbm.braga@gmail.com>
2021-11-03 16:59:49 -0300

    projeto organizado

--- Assets/_Project/Scripts/Managers/SceneGameManager.cs
+++ Assets/_Project/Scripts/Managers/SceneGameManager.cs
@@ -1,35 +1,6 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public sealed class SceneGameManager : MonoBehaviour
 {
-    public void Initialize()
-    {
-        
-    }
-
-    private void Awake()
-    {
-        Initialize();
-    }
-
-    //private void OnEnable()
-    //{
-    //    PlayerDieScript.OnPlayerDied += DelayedReloadScene;
-    //}
-
-    //private void OnDisable()
-    //{
-    //    PlayerDieScript.OnPlayerDied -= DelayedReloadScene;
-    //}
-
-    //private void DelayedReloadScene()
-    //{
-    //    //Invoke("Load", 5f);
-    //}
-
-    //private void Load()
-    //{
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    //}
+    
 }
